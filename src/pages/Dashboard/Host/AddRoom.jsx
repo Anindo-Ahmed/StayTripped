cf1e3397d0a77fcf6419a1b8af8b114bb1432936
Anindo <anindo.ah@gmail.com>
2024-05-27 00:33:48 +0600

    add room form applied

--- src/pages/Dashboard/Host/AddRoom.jsx
+++ src/pages/Dashboard/Host/AddRoom.jsx
@@ -1,11 +1,60 @@
+import { useState } from "react";
 import AddRoomForm from "../../../components/Form/AddRoomForm";
+import useAuth from "../../../hooks/useAuth";
 
 const AddRoom = () => {
-    return (
-        <div>
-            <AddRoomForm/>
-        </div>
-    );
+    const { user } = useAuth()
+  const [dates, setDates] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: "selection",
+  });
+
+// Date range handler
+
+const handleDates = item => {
+    console.log(item)
+    setDates(item.selection)
+}
+
+// handle form submit
+const handleSubmit = async e => {
+    e.preventDefault()
+    const form = e.target;
+    const location = form.location.value
+    const title = form.title.value
+    const price = form.price.value
+    const total_guest = form.total_guest.value
+    const bedrooms = form.bedrooms.value
+    const bathrooms = form.bathrooms.value
+    const description = form.description.value
+    const category = form.category.value
+    const from = ''
+    const to = ''
+    const image = form.image.files[0]
+    const host = {
+        name: user?.displayName,
+        email: user?.email,
+        photo: user?.photoURL
+    }
+
+    const newRoom = { location, title, price, total_guest, bedrooms, bathrooms, description, category}
+
+    console.log(newRoom);
+}
+
+
+
+
+
+  return (
+    <div>
+      <AddRoomForm
+      dates={ dates }
+      handleDates={ handleDates }
+      handleSubmit={handleSubmit}/>
+    </div>
+  );
 };
 
 export default AddRoom;
